77064586d429325864e80ffa9d98692015e031b9
ixistic <teeravipark@gmail.com>
2017-03-07 16:28:12 +0700

    second

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void mai(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
